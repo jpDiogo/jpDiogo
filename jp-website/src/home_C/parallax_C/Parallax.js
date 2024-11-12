a5a61f0073ac33919d3b5467ebb7c3a9b0440072
jpdiogoNOVA <jp.diogo@campus.fct.unl.pt>
2024-11-12 15:22:27 +0000

    Changed path to images

--- jp-website/src/home_C/parallax_C/Parallax.js
+++ jp-website/src/home_C/parallax_C/Parallax.js
@@ -4,10 +4,10 @@ const Parallax = () => {
     return (
         <section id="parallax">
             <div id="court">
-                <img src="/imgs/left_basket.svg" id= "left_basket_img" alt="left basket" />
-                <img src="/imgs/court.svg" id="court_img" alt="court" />
-                <img src="/imgs/right_basket.svg" id="right_basket_img" alt="right basket" />
-                <img src="/imgs/basketball.svg" id="basketball_img" alt="bouncing basketball" />
+                <img src={`${process.env.PUBLIC_URL}/imgs/left_basket.svg`} id= "left_basket_img" alt="left basket" />
+                <img src={`${process.env.PUBLIC_URL}/imgs/court.svg`} id="court_img" alt="court" />
+                <img src={`${process.env.PUBLIC_URL}/imgs/right_basket.svg`} id="right_basket_img" alt="right basket" />
+                <img src={`${process.env.PUBLIC_URL}/imgs/basketball.svg`} id="basketball_img" alt="bouncing basketball" />
             </div>
             
         </section>
